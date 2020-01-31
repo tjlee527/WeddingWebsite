4e0e016fad5613d4a80c5ea584245c3c3a646474
tjlee527 <tjlee527@gmail.com>
2020-01-31 14:33:51 -0800

    set up react for application

--- server/app.js
+++ server/app.js
@@ -7,7 +7,8 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('./client/dist'));
-app.get('/', (req, res) => res.send('Hello World'));
+
+// app.get('/', (req, res) => res.send('Hello World'));
 
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./client/src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "./client/dist/"),
+    filename: "bundle.js"
+  }
+};
