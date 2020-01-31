538a889fb25cb24e2ef94e7d5b807390d8a4bdba
tjlee527 <tjlee527@gmail.com>
2020-01-31 14:17:02 -0800

    added bodyPraser and serving static file

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('./client/dist'));
 app.get('/', (req, res) => res.send('Hello World'));
+
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
