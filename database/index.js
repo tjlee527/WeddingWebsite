b3c42d725ef6923187e9dc94020bee9d10c2ba48
tjlee527 <tjlee527@gmail.com>
2020-02-03 15:22:57 -0800

    added new routes for creating and getting tasks, set up mongodb

---
+++ database/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/weddingWebsite', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+const Schema = mongoose.Schema;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('db connected!');
+});
+
+
+const taskSchema = new Schema({
+  task: String,
+  status: Boolean
+});
+
+const Tasks = mongoose.model('Task', taskSchema);
+
+module.exports = Tasks;
--- server/app.js
+++ server/app.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Tasks = require('../database/index.js');
 
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static('./client/dist'));
 
-// app.get('/', (req, res) => res.send('Hello World'));
+app.get('/tasks', (req, res) => {
+  Tasks.find({}, (err, docs) => {
+    if (err) {
+      res.sendStatus(500);
+    }
+    res.send(docs);
+  })
+});
+
+app.post('/tasks', (req, res) => {
+  // console.log(req.body);
+  let desc = req.body.tasks[0];
+  let status = req.body.tasks[1];
+  let newTask = new Tasks({task: desc, status: status});
+  // console.log(newTask);
+  newTask.save((err, doc) => {
+    if (err) {
+      res.sendStatus(500);
+    }
+    res.sendStatus(200);
+  })
+})
 
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
